0ab059200a92e878f5c79fbe8bac6da74c320eb1
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-11 14:20:12 -0500

    fixed jitter

--- bigservo.py
+++ bigservo.py
@@ -3,11 +3,12 @@ from time import sleep
 from gpiozero.tools import sin_values
 from signal import pause
 
-from gpiozero.pins.pigpio import PiGPIOFactory
-pigpio_factory = PiGPIOFactory()
+# from gpiozero.pins.pigpio import PiGPIOFactory
+# pigpio_factory = PiGPIOFactory()
+#
+# servo = Servo(12, pin_factory=pigpio_factory)
 
-servo = Servo(12, pin_factory=pigpio_factory)
-
+servo = Servo(12)
 servo.source = sin_values()
 servo.source_delay = 0.03
 
