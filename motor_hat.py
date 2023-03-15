500527eabee33d062c6b7feb3997fe748ade87e1
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-15 19:20:20 -0400

    testing motor1

--- motor_hat.py
+++ motor_hat.py
@@ -1,13 +1,13 @@
 import time
-import board
+# import board
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
 
 while True:
-	kit.motor1.throttle = 1.0
+	kit.motor2.throttle = 1.0
 	time.sleep(0.5)
-	kit.motor1.throttle = 0
+	kit.motor2.throttle = 0
 	# time.sleep(0.5)
 	# kit.motor2.throttle = 1.0
 	# time.sleep(0.5)
