e7f855df1b07434894cde8938320b56d1419aead
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-15 19:09:51 -0400

    testing motor

--- motor_hat.py
+++ motor_hat.py
@@ -8,12 +8,12 @@ while True:
 	kit.motor1.throttle = 1.0
 	time.sleep(0.5)
 	kit.motor1.throttle = 0
-	time.sleep(0.5)
-	kit.motor2.throttle = 1.0
-	time.sleep(0.5)
-	kit.motor2.throttle = 0
-	time.sleep(0.5)
-	kit.motor3.throttle = 1.0
-	time.sleep(0.5)
-	kit.motor3.throttle = 0
+	# time.sleep(0.5)
+	# kit.motor2.throttle = 1.0
+	# time.sleep(0.5)
+	# kit.motor2.throttle = 0
+	# time.sleep(0.5)
+	# kit.motor3.throttle = 1.0
+	# time.sleep(0.5)
+	# kit.motor3.throttle = 0
 	time.sleep(1)
