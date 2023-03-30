477e4bb07f0d20de68ae1cea1cc9f290a94e9c72
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-30 16:52:20 -0400

    changed microservo0

--- microservo_0.py
+++ microservo_0.py
@@ -12,6 +12,26 @@ pigpio_factory = PiGPIOFactory()
 # servo = Servo(27, pin_factory=pigpio_factory)
 micro_servo = AngularServo(27, min_angle=-90, max_angle=90, pin_factory=pigpio_factory)
 
-sleep(1)
-micro_servo.angle = 0
-sleep(1)
+angle = 0
+angle_polarity = 0
+
+while True:
+    sleep(1)
+
+    #Micro Servo
+    ANG_UPPER_LIMIT = 140
+    ANG_LOWER_LIMIT = 60
+    INCRIMENT_BY = 10
+
+    if angle_polarity == 0:
+        angle += INCRIMENT_BY
+        if angle >= ANG_UPPER_LIMIT:
+            angle_polarity = 1
+    else:
+        angle -= INCRIMENT_BY
+        if angle <= ANG_LOWER_LIMIT:
+            angle_polarity = 0
+
+    ang = angle - 90
+    print(ang)
+    micro_servo.angle = ang
