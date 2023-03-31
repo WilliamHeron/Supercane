0acb4221a58267f39adf367e583075e98d45a39f
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-31 11:12:05 -0400

    added button4

--- run.py
+++ run.py
@@ -18,9 +18,10 @@ while True:
     input_state = GPIO.input(BUTTON_PIN)
     if input_state == False and set == 0:
         # subprocess.("/home/pi/securipi-rpicamtd.sh", shell=True)
-        p=subprocess.Popen( "/home/pi/securipi-picamtd.sh",shell=True,preexec_fn=os.setsid)
-        subprocess.call(['cd Supercane'])
-        subprocess.call(['python3 SupercaneMain.py'])
+        # p=subprocess.Popen( "/home/pi/securipi-picamtd.sh",shell=True,preexec_fn=os.setsid)
+        p=subprocess.Popen( "/home/pi/Supercane/SupercaneMain.py",shell=True,preexec_fn=os.setsid)
+        # subprocess.call(['cd Supercane'])
+        # subprocess.call(['python3 SupercaneMain.py'])
 
         print("set = 0")
         time.sleep(1)
