fd93671d72912bce5a78070a1ff1a4e505a626ca
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-03-31 11:03:41 -0400

    added button6

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ while True:
     if input_state == False and set == 0:
         # subprocess.("/home/pi/securipi-rpicamtd.sh", shell=True)
         p=subprocess.Popen( "/home/pi/securipi-picamtd.sh",shell=True,preexec_fn=os.setsid)
-        subprocess.call(['python SuperCaneMain.py'])
+        subprocess.call(['python3 SuperCaneMain.py'])
 
         print("set = 0")
         time.sleep(1)
