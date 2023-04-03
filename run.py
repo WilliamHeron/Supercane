9adb631dd6632499afaedad251c2c87538e03241
WilliamHeron <71040868+WilliamHeron@users.noreply.github.com>
2023-04-03 13:55:12 -0400

    changed run code6

--- run.py
+++ run.py
@@ -29,13 +29,49 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 import os
-import thread
+import threading
+import logging
 
 BUTTON_PIN = 18
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 set = 0
+
+
+lock = None
+
+
+
+class RunPython(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+        # self._lock = toggle_lock
+
+    def run(self):
+        subprocess.run(["python3", "SupercaneMain.py"], capture_output=True, text=True)
+
+        # for message in self._args:
+        #     if self._lock:
+        #         self._lock.acquire()
+        #     print(message)
+        #     if self._lock:
+        #         self._lock.release()
+        #     time.sleep(self._kwargs.get("delay", 1.0))
+
+# mp1 = MessagePrinter("Hello", "Good day!", lock=lock)
+# mp2 = MessagePrinter("A", "B", "C", delay=3, lock=lock)
+# mp1.start()
+# mp2.start()
+# mp1.join()
+# mp2.join()
+
+
+
+
+py_script = RunPython()
+
 while True:
     input_state = GPIO.input(BUTTON_PIN)
     if input_state == False and set == 0:
@@ -46,13 +82,7 @@ while True:
         # subprocess.call(['python3 SupercaneMain.py'])
         # subprocess.run(["python", "SupercaneMain.py"])
 
-        try:
-            thread.start_new_thread(subprocess.run(["python3", "SupercaneMain.py"], capture_output=True, text=True))
-
-        except:
-            print("couldn't start thread!")
-            pass
-
+        py_script.start()
         # cane = Supercane()
         print("set = 0")
         time.sleep(1)
@@ -61,11 +91,11 @@ while True:
     Input_state = GPIO.input(BUTTON_PIN)
     if input_state == False and set == 1:
         # cane = Supercane()
-        os.killpg(result.pid, signal.SIGTERM)
+        py_script.stop()
 
         result = subprocess.run(["python3", "RESET.py"], capture_output=True, text=True)
         sleep(0.5)
-        os.killpg(result.pid, signal.SIGTERM)
+        # os.killpg(result.pid, signal.SIGTERM)
 
         print(result.stdout)
 
